35a15a6fe3ff8a63fd636d11ce996940694e06a8
Dylan <dylanpdx@gmail.com>
2026-02-13 15:55:32 +0000

    Update video embeds

--- src/embedConverters/videoConverter.ts
+++ src/embedConverters/videoConverter.ts
@@ -5,7 +5,7 @@ import zlib from 'zlib';
 import { promisify } from 'util';
 import getConfig from "../config";
 const gzip = promisify(zlib.gzip);
-
+const re = /video\.bsky\.app\/watch\/(did.*?)\//gm;
 export default async function ConvertVideo(video:AppBskyEmbedVideo.View) : Promise<VideoMedia[]>{
     const config = getConfig();
 
@@ -13,17 +13,12 @@ export default async function ConvertVideo(video:AppBskyEmbedVideo.View) : Promi
         return [];
     }
 
-    // logic to get video url from m3u8
-    const m3u8 = await fetch(video.playlist).then((res)=>res.text());
-    // yes, I know the m3u8's bsky serves are very simple and I could parse them myself
-    const parsedData = parse(m3u8) as types.MasterPlaylist;
+    const didMatch = re.exec(video.playlist);
+    if (!didMatch || didMatch.length < 2){
+        return [];
+    }
 
-    const hdVideoM3u8 = parsedData.variants.reduce((prev, current) => (prev.bandwidth > current.bandwidth) ? prev : current);
-    const hdUri = video.playlist.replace(/[^/]*$/, hdVideoM3u8.uri);
-
-    // pass it to somewhere where we can call the following:
-    // ffmpeg -i $hdUri -vcodec copy -acodec copy out.mp4
-    const apiUrl = hdUri.replace("https://video.bsky.app",config.videoConvertApi).replace(".m3u8","");
+    const url = `https://bsky.social/xrpc/com.atproto.sync.getBlob?did=${didMatch[1]}&cid=${video.cid}`;
 
     const media:VideoMedia[] = [
     {
@@ -31,7 +26,7 @@ export default async function ConvertVideo(video:AppBskyEmbedVideo.View) : Promi
         size:{width:video.aspectRatio?.width as number,height:video.aspectRatio?.height as number},
         thumbnail_url:video.thumbnail,
         type:"video",
-        url:apiUrl
+        url:url
     }];
     return media;
 }
